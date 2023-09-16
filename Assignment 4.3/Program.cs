eb5953992f14fd3ab183e7fb6210b761568a731a
Sunil Dagur <sunildagur566@gmail.com>
2023-09-16 18:40:29 +0530

    Add project files.

---
+++ Assignment 4.3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_4._3
+{
+    using System;
+
+    class Person
+    {
+        
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        
+        public string FullNameInUpperCase
+        {
+            get
+            {
+                
+                return $"{FirstName} {LastName}".ToUpper();
+            }
+        }
+
+        
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            
+            Person person = new Person("Sunil", "Dagur");
+
+            
+            string fullNameUpperCase = person.FullNameInUpperCase;
+
+            
+            Console.WriteLine("Full Name in Uppercase: " + fullNameUpperCase);
+        }
+    }
+
+}
